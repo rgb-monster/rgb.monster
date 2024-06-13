7aec9b16da68a93197f650816b0bcf222133c8cd
Tom Striker <toms.baugis@gmail.com>
2024-06-13 11:53:35 +0100

    try shutting up warnings in chrome

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -14,10 +14,31 @@ export default defineConfig({
         ["link", {rel: "icon", href: "/favicon.png"}],
         ["link", {rel: "preconnect", href: "https://fonts.googleapis.com"}],
         ["link", {rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: ""}],
-        ["link", {href: "https://fonts.googleapis.com/css2?family=Hammersmith+One&display=swap", rel: "stylesheet"}],
-        ["link", {href: "https://fonts.googleapis.com/css2?family=Lato&display=swap", rel: "stylesheet"}],
+        [
+            "link",
+            {
+                href: "https://fonts.googleapis.com/css2?family=Hammersmith+One&display=swap",
+                rel: "stylesheet",
+                crossOrigin: "anonymous",
+            },
+        ],
+        [
+            "link",
+            {
+                href: "https://fonts.googleapis.com/css2?family=Lato&display=swap",
+                rel: "stylesheet",
+                crossOrigin: "anonymous",
+            },
+        ],
 
-        ["link", {href: "https://fonts.googleapis.com/icon?family=Material+Icons", rel: "stylesheet"}],
+        [
+            "link",
+            {
+                href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+                rel: "stylesheet",
+                crossOrigin: "anonymous",
+            },
+        ],
 
         [
             "script",
